e24229cf601781c91487863f0a0d129c19e057e4
bajcsiadel <33290209+bajcsiadel@users.noreply.github.com>
2017-11-03 12:00:50 +0200

    Update hello.rs

--- Test/hello.rs
+++ Test/hello.rs
@@ -1,3 +1,10 @@
+/*
+ * install rust to your computer
+ * $ rustc hello.rs
+ * $ ./hello (<- in Linux bash)
+ * > .\hello.exe (<- in Windows cmd)
+ */
+
 fn main() {
     println!("Hello World!");
 }
