d48a078e6e96fb5e7ef469ba755627b05d28238a
Bajcsi Adel <bajcsiadel@yahoo.com>
2017-11-19 16:26:37 +0200

    AB: a util file which contains the domain, username and password to connect with ssh to the cars raspberry

---
+++ Test/utils.py
@@ -0,0 +1,9 @@
+class Utils:
+    def get_hostname(self):
+        return '192.168.1.116'
+
+    def get_username(self):
+        return 'pi'
+
+    def get_password(self):
+        return 'raspberry'
