0f67672f1ef1a00d2e41a8b9fb84ef1916bf27e3
Lazar Botond <botiko123@yahoo.com>
2018-01-13 00:52:37 +0200

    cameratest

---
+++ picamera/piCamera.py
@@ -0,0 +1,26 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+import datetime
+import cv2
+ 
+class PiiCamera:
+	def __init__(self, resolution=(384, 216)):
+		self.camera = PiCamera()
+		self.camera.resolution = resolution
+		self.stopped = False
+	def start(self):
+		Thread(target=self.update, args=()).start()
+		return self
+ 
+	def update(self):
+		while self.stopped == False
+			picture = '/etc/img/' + str(datetime.datetime.utcnow()) + '.jpg'
+ 			self.camera.capture(picture)
+
+ 		if self.stopped:
+			self.camera.close()
+			return
+ 
+	def stop(self):
+		self.stopped = True
