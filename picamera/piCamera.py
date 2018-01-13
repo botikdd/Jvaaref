d09e13532cd292f2f794ff8f2607f8cfb1589e0b
Lazar Botond <botiko123@yahoo.com>
2018-01-13 12:04:57 +0200

    rewrited picamera to extended threads

--- picamera/piCamera.py
+++ picamera/piCamera.py
@@ -4,23 +4,19 @@ from threading import Thread
 import datetime
 import cv2
  
-class PiiCamera:
-	def __init__(self, resolution=(384, 216)):
-		self.camera = PiCamera()
-		self.camera.resolution = resolution
-		self.stopped = False
-	def start(self):
-		Thread(target=self.update, args=()).start()
-		return self
- 
-	def update(self):
-		while self.stopped == False
-			picture = '/etc/img/' + str(datetime.datetime.utcnow()) + '.jpg'
- 			self.camera.capture(picture)
+class PiiCamera(threading.Thread):
+    def __init__(self, resolution=(384, 216)):
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.stopped = False
+    def start(self):
+        while self.stopped == False:
+       	    picture = '/etc/img/' + str(datetime.datetime.utcnow()) + '.jpg'
+       	    self.camera.capture(picture)
 
- 		if self.stopped:
-			self.camera.close()
-			return
+        if self.stopped:
+            self.camera.close()
+            return
  
-	def stop(self):
-		self.stopped = True
+    def stop(self):
+        self.stopped = True
